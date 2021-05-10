42c92839b6974910bcd2dbebbf200382b87548b9
David Hin <hello@davidhin.com>
2021-05-10 11:07:12 +0930

    Add verbosity

--- gnnproject/helpers/old_joern.py
+++ gnnproject/helpers/old_joern.py
@@ -20,13 +20,13 @@ def subprocess_cmd(command: str, verbose: int = 0):
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
     )
     output = process.communicate()
-    if verbose > 0:
+    if verbose > 1:
         gp.debug(output[0].decode())
         gp.debug(output[1].decode())
 
 
 def run_joern_old(
-    file_in: str, dataset: str, joern_old_path: str, save: bool = True
+    file_in: str, dataset: str, joern_old_path: str, save: bool = True, verbose=0
 ) -> pd.DataFrame:
     """Run joern (old version) on a given file.
 
@@ -42,7 +42,8 @@ def run_joern_old(
         gp.get_dir(save_out)
 
     if os.path.exists(save_out / "nodes.csv"):
-        gp.debug(f"Already completed {filename}")
+        if verbose > 0:
+            gp.debug(f"Already completed {filename}")
         if not save:
             return [
                 pd.read_csv(save_out / "nodes.csv", sep="\t"),
@@ -53,13 +54,17 @@ def run_joern_old(
     tmppath = gp.get_dir(gp.interim_dir() / str(dataset + "_oldjoern") / filename)
     shutil.copy(file_in, tmppath)
 
-    subprocess_cmd(f"cd {tmppath}; mkdir tmp; mv *.c tmp; {joern_old_path} tmp")
+    subprocess_cmd(
+        f"cd {tmppath}; mkdir tmp; mv *.c tmp; {joern_old_path} tmp", verbose=verbose
+    )
 
     ret = []
     for f in glob(f"{tmppath}/parsed/tmp/{Path(file_in).name}/*"):
         ret.append(pd.read_csv(f, sep="\t"))
         if save:
             shutil.move(f, save_out)
+    if verbose > 0:
+        gp.debug(f"Saved to {save_out}")
 
     shutil.rmtree(tmppath)
     if not save:
