3faa43bdda891a4584898c684a0337dfa6530e79
David Hin <hello@davidhin.com>
2021-05-12 11:02:00 +0930

    Add edge types constant

---
+++ gnnproject/helpers/constants.py
@@ -0,0 +1,15 @@
+EDGE_TYPES = {
+    "CONTROLS": 0,
+    "DECLARES": 1,
+    "DEF": 2,
+    "DOM": 3,
+    "FLOWS_TO": 4,
+    "IS_AST_PARENT": 5,
+    "IS_CLASS_OF": 6,
+    "IS_FILE_OF": 7,
+    "IS_FUNCTION_OF_AST": 8,
+    "IS_FUNCTION_OF_CFG": 9,
+    "POST_DOM": 10,
+    "REACHES": 11,
+    "USE": 12,
+}
