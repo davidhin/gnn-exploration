bd6f90329e8d3f422b569f71b3f7fa9fe0321b1e
David Hin <hello@davidhin.com>
2021-05-17 10:50:39 +0930

    Update jobnames

--- hpc/basic_ggnn/run_variation_5.sh
+++ hpc/basic_ggnn/run_variation_5.sh
@@ -6,9 +6,9 @@
 #SBATCH --mem=16GB
 #SBATCH --gres=gpu:1
 #SBATCH --array=1-10
-#SBATCH --err="hpc/logs/basic_ggnn_%a.info"
-#SBATCH --output="hpc/logs/basic_ggnn_%a.info"
-#SBATCH --job-name="basic_ggnn_%a"
+#SBATCH --err="hpc/logs/bgv5_%a.info"
+#SBATCH --output="hpc/logs/bgv5_%a.info"
+#SBATCH --job-name="bgv5_%a"
 
 # Setup Python Environment
 module load Singularity
