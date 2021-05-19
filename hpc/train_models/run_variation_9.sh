a10b3dac6c8aea7aac206586b772af8b58cc5f4e
David Hin <hello@davidhin.com>
2021-05-20 08:53:21 +0930

    Reduce devign batch sizes to 16

--- hpc/train_models/run_variation_9.sh
+++ hpc/train_models/run_variation_9.sh
@@ -15,4 +15,4 @@ module load Singularity
 module load CUDA/10.2.89
 
 # Start singularity instance
-singularity exec --nv main.simg python gnnproject/analysis/train_ggnn_basic.py --variation cpg --model devign --batch_size 32
+singularity exec --nv main.simg python gnnproject/analysis/train_ggnn_basic.py --variation cpg --model devign --batch_size 16
