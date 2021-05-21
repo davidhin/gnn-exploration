efa6af71530ad82507900558890f7d0a4860a35a
David Hin <hello@davidhin.com>
2021-05-21 10:34:24 +0930

    Refactor train_ggnn_basic

--- gnnproject/analysis/train_ggnn_basic.py
+++ gnnproject/analysis/train_ggnn_basic.py
@@ -20,29 +20,10 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--dataset",
-        default="devign_ffmpeg_qemu",
-        choices=["devign_ffmpeg_qemu"],
-    )
-    parser.add_argument("--variation", default="cfg", choices=["cfg", "cfgdfg", "cpg"])
-    parser.add_argument("--batch_size", default=64, type=int)
-    parser.add_argument("--learn_rate", default=0.0001, type=float)
-    parser.add_argument("--in_num", default=169, type=int)
-    parser.add_argument("--out_num", default=200, type=int)
-    parser.add_argument("--split_seed", default=randrange(5), type=int)
-    parser.add_argument("--patience", default=30, type=int)
-    parser.add_argument("--noggnn", action="store_true")
-    parser.add_argument("--model", default="ggnnsum", choices=["devign", "ggnnsum"])
-    try:
-        args = parser.parse_args()
-    except:
-        args = parser.parse_args([])
-    gp.debug(args)
 
-    # %% Load own feature extracted graphs
+def main(args):
+    """Train a basic GGNN."""
+    # Load own feature extracted graphs
     dgl_proc_files = glob(
         str(gp.processed_dir() / f"{args.dataset}_dgl_{args.variation}/*")
     )
@@ -75,7 +56,7 @@ if __name__ == "__main__":
             )
         sys.exit()
 
-    # %% Save splits
+    # Save splits
     train, val, test = dglh.train_val_test(dgl_proc_files, seed=args.split_seed)
     records = []
     for s, n in [("train", train), ("val", val), ("test", test)]:
@@ -106,7 +87,7 @@ if __name__ == "__main__":
     gp.debug(Counter([int(i) for i in valset.labels]))
     gp.debug(Counter([int(i) for i in testset.labels]))
 
-    # %% Get dataloader
+    # %Get dataloader
     dl_args = {
         "batch_size": args.batch_size,
         "shuffle": True,
@@ -116,7 +97,7 @@ if __name__ == "__main__":
     val_loader = DataLoader(valset, **dl_args)
     test_loader = DataLoader(testset, **dl_args)
 
-    # %% Get DL model
+    # Get DL model
     if args.model == "ggnnsum":
         model = dglh.BasicGGNN(args.in_num, args.out_num)
     if args.model == "devign":
@@ -127,10 +108,10 @@ if __name__ == "__main__":
     savepath = savedir / f"best_basic_ggnn_{ID}.bin"
     model = model.to("cuda")
 
-    # %% Start Tensorbaord
+    # Start Tensorbaord
     writer = SummaryWriter(savedir / "best_basic_ggnn" / ID)
 
-    # %% Train model
+    # Train model
     dglh.train(
         model=model,
         train_loader=train_loader,
@@ -143,18 +124,18 @@ if __name__ == "__main__":
     )
     torch.cuda.empty_cache()
 
-    # %% Evaluate scores on splits
+    # Evaluate scores on splits
     model.load_state_dict(torch.load(savepath))
     ggnn_results_train = dglh.eval_model(model, train_loader, loss_func, True)
     ggnn_results_val = dglh.eval_model(model, val_loader, loss_func, True)
     ggnn_results_test = dglh.eval_model(model, test_loader, loss_func, True)
 
-    # %% Get and save intermediate representations
+    # Get and save intermediate representations
     train_graph_rep = dglh.get_intermediate(model, train_loader)
-    val_graph_rep = dglh.get_intermediate(model, val_loader)
     test_graph_rep = dglh.get_intermediate(model, test_loader)
 
     # %% Resample
+    # val_graph_rep = dglh.get_intermediate(model, val_loader)
     # all_reps = train_graph_rep + val_graph_rep + test_graph_rep
     # train_graph_rep, test_graph_rep = dglh.train_val_test(
     #     all_reps, train_ratio=0.8, val_ratio=0, test_ratio=0.2, seed=args.split_seed
@@ -169,13 +150,13 @@ if __name__ == "__main__":
     ) as f:
         pkl.dump(test_graph_rep, f)
 
-    # %% Get representation learning results
+    # Get representation learning results
     rlearning_results_train, rlearning_results_test = rlm.representation_learning(
         gp.processed_dir() / "dl_models" / f"basic_ggnn_{ID}_hidden_train.pkl",
         gp.processed_dir() / "dl_models" / f"basic_ggnn_{ID}_hidden_test.pkl",
     )
 
-    # %% Save results
+    # Save results
     final_savedir = gp.get_dir(gp.outputs_dir())
     with open(final_savedir / "basic_ggnn_results.csv", "a") as f:
         f.write(
@@ -191,3 +172,29 @@ if __name__ == "__main__":
             )
             + "\n"
         )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        default="devign_ffmpeg_qemu",
+        choices=["devign_ffmpeg_qemu"],
+    )
+    parser.add_argument("--variation", default="cfg", choices=["cfg", "cfgdfg", "cpg"])
+    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--learn_rate", default=0.0001, type=float)
+    parser.add_argument("--in_num", default=169, type=int)
+    parser.add_argument("--out_num", default=200, type=int)
+    parser.add_argument("--split_seed", default=randrange(5), type=int)
+    parser.add_argument("--patience", default=30, type=int)
+    parser.add_argument("--noggnn", action="store_true")
+    parser.add_argument("--model", default="ggnnsum", choices=["devign", "ggnnsum"])
+    try:
+        args = parser.parse_args()
+    except:
+        args = parser.parse_args([])
+    gp.debug(args)
+    main(args)
+
+# %%
